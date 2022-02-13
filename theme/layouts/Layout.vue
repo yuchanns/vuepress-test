e6303c06073f7884db4edc491aa93e089d54faf3
yuchanns <airamusume@gmail.com>
2022-02-13 17:55:38 +0800

    update css

--- theme/layouts/Layout.vue
+++ theme/layouts/Layout.vue
@@ -1,5 +1,5 @@
 <template>
-<div style="margin: 0 auto;">
+<div class="wrapper">
     <RouterLink to="/">Home</RouterLink>
     <RouterLink to="/about">About</RouterLink>
     <p>Hello Layout.</p>
@@ -9,7 +9,18 @@
 
 <style>
 body {
-    text-align: center;
+    margin: 0;
+    color: #000000de;
+    font-size: 1rem;
+    line-height: 1.5;
+    letter-spacing: .00938em;
+}
+
+.wrapper {
+    margin-left: auto;
+    margin-right: auto;
+    max-width: 39rem;
+    padding: 2.4375rem 1.21875rem;
 }
 
 a {
